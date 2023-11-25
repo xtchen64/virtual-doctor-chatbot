85f5c41dd4fe1e7d3cfad342c21a101e3d326bc3
xtchen64 <xtchen64@gmail.com>
2023-11-24 23:49:40 -0800

    Completed initial website demo.

--- website/script.js
+++ website/script.js
@@ -1,5 +1,7 @@
-document.getElementById('send-button').addEventListener('click', function() {
+// Function to handle sending of message
+function sendMessage() {
     var userInput = document.getElementById('message-input').value;
+    if (!userInput.trim()) return; // Prevent sending empty messages
     document.getElementById('message-input').value = '';
 
     var chatContainer = document.getElementById('chat-container');
@@ -14,4 +16,15 @@ document.getElementById('send-button').addEventListener('click', function() {
         chatContainer.innerHTML += '<div class="message bot-message">' +
                                    '<div class="avatar doctor-avatar"></div>' + botResponse + '</div>';
     }, 1000);
+}
+
+// Event listener for the Send button
+document.getElementById('send-button').addEventListener('click', sendMessage);
+
+// Event listener for the Enter key in the input field
+document.getElementById('message-input').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault(); // Prevent the default action to stop form submission
+        sendMessage();
+    }
 });
